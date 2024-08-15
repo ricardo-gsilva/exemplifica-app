423a16bbf580e60c2cc0ef373b37cb3c82f31854
Ricardo Gomes <54490243+ricardo-gsilva@users.noreply.github.com>
2024-08-15 19:51:21 -0300

    retirando import

--- lib/widgets/organisms/buttons_grid_calculators.dart
+++ lib/widgets/organisms/buttons_grid_calculators.dart
@@ -7,7 +7,6 @@ import 'package:exemplifica/screens/calculadoras/calc_porcentagem.dart';
 import 'package:exemplifica/screens/calculadoras/calc_regra_de_3.dart';
 import 'package:exemplifica/screens/calculadoras/cald_fatorial.dart';
 import 'package:exemplifica/screens/calculadoras/cald_mdc.dart';
-import 'package:exemplifica/screens/calculadoras/cald_tabuada.dart';
 import 'package:exemplifica/utils/constants/core_strings.dart';
 import 'package:exemplifica/widgets/page/calc_tabuada_page.dart';
 import 'package:flutter/material.dart';
