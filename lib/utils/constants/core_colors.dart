3ba88dec07bfb59e4a3e30fcb035a4a6e8f41968
Ricardo Gomes <54490243+ricardo-gsilva@users.noreply.github.com>
2024-08-20 16:45:42 -0300

    refatorando text field e excluindo widgets sem uso

--- lib/utils/constants/core_colors.dart
+++ lib/utils/constants/core_colors.dart
@@ -8,5 +8,13 @@ class CoreColors {
   static const Color textAlert = Color(0xFFFF0000);
   static const Color appBarColor = Color (0xFF03A9F4);
   static const Color btnColor = Color (0xFF81D4FA);
+
+  static const Color borderAlert = Color(0xFFFF0000);
+  static const Color errorAlert = Color(0xFFFF0000);
+  static const Color enableBorder = Color(0xFFFFFFFF);
+  static const Color focusBorder = Color(0xFFFFC107);
+  static const Color borderSide = Color(0xFF000000);
+  static const Color fillColorTextField = Color(0xFFFFFFFF);
+  
   
 }
--- lib/widgets/atoms/custom_text_field.dart
+++ lib/widgets/atoms/custom_text_field.dart
@@ -1,6 +1,6 @@
+import 'package:exemplifica/utils/constants/core_colors.dart';
+import 'package:exemplifica/utils/constants/core_fontSize.dart';
 import 'package:flutter/material.dart';
-
-import 'package:exemplifica/components/text_field_input.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
@@ -17,12 +17,32 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFieldInput(
-      title: title,
-      hintText: hintText,
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          filled: true,
+          fillColor: CoreColors.fillColorTextField,
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: CoreColors.borderSide)),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: CoreColors.focusBorder)),
+          errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: CoreColors.borderAlert)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1, color: CoreColors.enableBorder)),
+          errorStyle: TextStyle(color: CoreColors.errorAlert),
+          focusedErrorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: CoreColors.borderAlert)),
+          counterStyle: const TextStyle(color: CoreColors.textPrimary),
+          hintText: hintText,
+          labelText: "",
+          labelStyle: TextStyle(color: CoreColors.textPrimary)),
+      textAlign: TextAlign.center,
+      textAlignVertical: TextAlignVertical.bottom,
+      style:
+          TextStyle(color: CoreColors.textPrimary, fontSize: CoreFontSize.h_22),
       controller: controller,
       maxLength: 5,
-      mainAxisAlignment: mainAxisAlignment,
     );
   }
 }
