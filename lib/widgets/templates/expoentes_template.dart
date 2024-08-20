0c77f4371f0da1c1056832ea851b16b52f2cdb25
Ricardo Gomes <54490243+ricardo-gsilva@users.noreply.github.com>
2024-08-20 17:19:04 -0300

    refatorando lista de conteudo expoentes

--- lib/widgets/templates/expoentes_template.dart
+++ lib/widgets/templates/expoentes_template.dart
@@ -1,13 +1,15 @@
+import 'package:exemplifica/model/assets_model.dart';
+import 'package:exemplifica/utils/constants/core_strings_assets.dart';
+import 'package:exemplifica/widgets/molecules/content_list.dart';
 import 'package:flutter/material.dart';
 import 'package:exemplifica/utils/constants/strings/strings_expoentes.dart';
-import 'package:exemplifica/widgets/atoms/custom_image_asset.dart';
-import 'package:exemplifica/widgets/atoms/custom_text.dart';
-
 class ExpoentesTemplate extends StatelessWidget {
   final double width;
+  final double height;
   const ExpoentesTemplate({
     Key? key,
     required this.width,
+    required this.height,
   }) : super(key: key);
 
   @override
@@ -15,36 +17,33 @@ class ExpoentesTemplate extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          CustomText(
-            title: CoreStringsExpoentes.text1_expoentes,
-          ),
-          CustomImageAsset(
-            asset: "images/expoentes/expoentes_2.png",
-            width: width * 0.9,
-          ),
-          CustomText(
-            title: CoreStringsExpoentes.text2_expoentes,
-          ),
-          CustomImageAsset(
-            asset: "images/expoentes/expoentes_3.png",
-            width: width * 0.25,
-          ),
-          CustomText(
-            title: CoreStringsExpoentes.text3_expoentes,
-          ),
-          CustomImageAsset(
-            asset: "images/expoentes/expoentes_4.png",
-            width: width * 0.2,
-          ),
-          CustomText(
-            title: CoreStringsExpoentes.text4_expoentes,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40),
-            child: CustomImageAsset(
-              asset: "images/expoentes/expoentes_5.png",
-              width: width * 0.2,
-            ),
+          ContentList(
+            stringsAndAssets: [
+              StringsAndAssetsModel(
+                title: CoreStringsExpoentes.text1_expoentes,
+              ),
+              StringsAndAssetsModel(
+                  title: CoreStringsAssets.expoentes_assets_2,
+                  height: height * 0.05),
+              StringsAndAssetsModel(
+                title: CoreStringsExpoentes.text2_expoentes,
+              ),
+              StringsAndAssetsModel(
+                  title: CoreStringsAssets.expoentes_assets_3,
+                  height: height * 0.05),
+              StringsAndAssetsModel(
+                title: CoreStringsExpoentes.text3_expoentes,
+              ),
+              StringsAndAssetsModel(
+                  title: CoreStringsAssets.expoentes_assets_4,
+                  height: height * 0.05),
+              StringsAndAssetsModel(
+                title: CoreStringsExpoentes.text4_expoentes,
+              ),
+              StringsAndAssetsModel(
+                  title: CoreStringsAssets.expoentes_assets_5,
+                  height: height * 0.05),
+            ],
           ),
         ],
       ),
